9b91128cefa1f72124cabf2e0969514beef2184c
sebasgrasso <102562676+sebasgrasso@users.noreply.github.com>
2023-11-05 14:33:14 -0300

    routing in progress

--- src/homePage/homePage.tsx
+++ src/homePage/homePage.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import LeftBar from './components/leftBar.tsx';
 import Feed from './components/feed.tsx';
 import RightBar from './components/rightBar.tsx';
 import { Container, Grid } from '@mui/material';
 import './css/homePage.ts';
+import { useParams } from 'react-router-dom';
+import { useAuth } from '../auth/hooks/useAuth.ts';
 
 
 export const HomePage = () =>{
+  const { instanciaX } = useParams<{ instanciaX: string }>();
+  const { handleLogout } = useAuth();
+
+  useEffect(() => {
+    handleLogout();
+    console.log("cerre sesion porque cambie de instancia");
+    
+  }, [instanciaX]);
+  
     return (
         <div style={{ backgroundColor: "#191b22", minHeight: "100vh" }}>
         <Container style={{ backgroundColor: "#191b22", minHeight: "100vh" }}>
@@ -25,5 +36,3 @@ export const HomePage = () =>{
         </div>
       );
 }
-
- 
